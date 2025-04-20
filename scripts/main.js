fcd0b4773b65727c1402c9eecf956fa5e6957a0c
Jeong40 <158138039+Jeong40@users.noreply.github.com>
2025-04-20 23:40:52 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -8,24 +8,20 @@ var popBtn = document.getElementById("popBtn");
 var popClose = document.getElementById("popClose");
 
 goodPhotosBox.onclick = function(){
-  pop.style.zIndex = "10";
   popTitle.innerHTML = "好看的图片";
   popContent.innerHTML = "网上搜集的资源，侵权删（12大概率是ai作图，所以我把它稍微修改了一下，以显得更自然些,封面图就是修改后的12）";
   popBtn.href = "https://www.123684.com/s/EVPZjv-cjbRd";
   popBtn.innerHTML = "查看";
+  pop.style.top = "50%";
 }
 calculator.onclick = function(){
-  pop.style.zIndex = "10";
   popTitle.innerHTML = "计算器";
   popContent.innerHTML = "Jeong用javascript开发的计算器，仍在更新中";
   popBtn.href = "./pages/calculator.html";
   popBtn.innerHTML = "打开";
+  pop.style.top = "50%";
 }
 
 popClose.onclick = function(){
-  pop.style.zIndex = "-2";
-  popTitle.innerHTML = "";
-  popContent.innerHTML = "";
-  popBtn.href = "./";
-  popBtn.innerHTML = "";
+  pop.style.top = "150vh";
 }
