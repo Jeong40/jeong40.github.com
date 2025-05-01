e06abe66a796e3091939cb5be542199bf1e43f38
Jeong40 <158138039+Jeong40@users.noreply.github.com>
2025-05-01 22:42:29 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,5 @@
 var goodPhotosBox = document.getElementById("good-photos-box");
 var calculator = document.getElementById("calculator");
-var mcBox = document.getElementById("mc-box");
 
 var pop = document.getElementById("pop");
 var popTitle = document.getElementById("popTitle");
@@ -13,24 +12,13 @@ goodPhotosBox.onclick = function(){
   popContent.innerHTML = "网上搜集的资源，侵权删（12大概率是ai作图，所以我把它稍微修改了一下，以显得更自然些,封面图就是修改后的12）";
   popBtn.href = "https://www.123684.com/s/EVPZjv-cjbRd";
   popBtn.innerHTML = "查看";
-  popBtn.target = "_blank"
   pop.style.top = "50%";
-  
 }
 calculator.onclick = function(){
   popTitle.innerHTML = "计算器";
-  popContent.innerHTML = "自己用javascript写的计算器，仍在更新中";
+  popContent.innerHTML = "Jeong用javascript开发的计算器，仍在更新中";
   popBtn.href = "./pages/calculator.html";
   popBtn.innerHTML = "打开";
-  popBtn.target = "_self"
-  pop.style.top = "50%";
-}
-mcBox.onclick = function(){
-  popTitle.innerHTML = "这是什么？？？";
-  popContent.innerHTML = "项脊轩……但是是在MC里？？？";
-  popBtn.href = "https://www.bilibili.com/video/BV16p5yzjEmB/";
-  popBtn.innerHTML = "观看";
-  popBtn.target = "_blank";
   pop.style.top = "50%";
 }
 
