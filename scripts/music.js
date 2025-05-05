fdf2a6ccd151a0f66794ae9843bf7ec09d5889c3
Jeong40 <158138039+Jeong40@users.noreply.github.com>
2025-05-05 19:22:50 +0800

    Add files via upload

--- scripts/music.js
+++ scripts/music.js
@@ -51,9 +51,8 @@ function startPlaying(){
     isPlaying = false;
     music.pause()
     clearInterval(interval);
-    timeDisplay.innerHTML = "Loading";
-     playing.style.color = "rgba(0,0,0,0.0)";
-     paused.style.color = "rgba(0,0,0,1.0)";
+    playing.style.color = "rgba(0,0,0,0.0)";
+    paused.style.color = "rgba(0,0,0,1.0)";
   }else{
     isPlaying = true;
     interval = setInterval(rotate,15);
@@ -141,7 +140,6 @@ music.addEventListener("loadedmetadata",function(){
     interval = setInterval(rotate,15);
   }
 });
-
 
 
 
